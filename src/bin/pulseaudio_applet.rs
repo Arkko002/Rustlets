d1b6ebc8ff3f69357ddd1e7f4f1d7775be2c1bfc
Arkadiusz Kołodziejek <arkko002@gmail.com>
2021-08-07 23:43:36 +0200

    Add Sink and Source structs

--- src/bin/pulseaudio_applet.rs
+++ src/bin/pulseaudio_applet.rs
@@ -1,8 +1,24 @@
+#[path = "../pulse_dbus.rs"]
+mod pulse_dbus;
+
+use crate::pulse_dbus::PulesDbusClient;
+use gtk::{prelude::*, Application, ApplicationWindow};
+
 fn main() {
+    let app = Application::builder()
+        .application_id("org.rustlets.PulesAudio")
+        .build();
 
-}
+    app.connect_activate(|app| {
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("PulseAudio Rustlet")
+            .build();
 
+        win.show_all();
+    });
 
-fn create_gui() {
-
+    app.run();
 }
--- src/pulse_dbus.rs
+++ src/pulse_dbus.rs
@@ -2,26 +2,48 @@ use std::os::unix::net::UnixStream;
 
 use zbus::{export::zvariant::ObjectPath, Connection, Error, Proxy};
 
-//#[dbus_proxy]
-//trait Address {
-//fn notify(
-//&self,
-//address: &str,
-//) -> zbus::Result<u32>;
-//}
-
-pub struct PulesDbusClient {
+pub struct PulesDbusClient<'a> {
     conn: Connection,
+    pub sinks: Vec<Sink<'a>>,
+    pub sources: Vec<Source<'a>>,
 }
 
-impl PulesDbusClient {
+pub struct Sink<'a> {
+    path: ObjectPath<'a>,
+    name: String,
+    volume: u32,
+    mute: bool,
+}
+
+pub struct Source<'a> {
+    path: ObjectPath<'a>,
+    name: String,
+    volume: u32,
+    mute: bool,
+}
+
+impl<'a> PulesDbusClient<'a> {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
         let adr: String = PulesDbusClient::get_server_address()?;
 
         let soc_stream = UnixStream::connect(adr)?;
         let conn = Connection::new_unix_client(soc_stream, false)?;
 
-        Ok(Self { conn })
+        let proxy = Proxy::new(
+            &conn,
+            "org.PulseAudio1",
+            "/org/pulseaudio/server_lookup1",
+            "org.PulseAudio.ServerLookup1",
+        )?;
+
+        let sinks = PulesDbusClient::get_sinks(&proxy, &conn)?;
+        let sources = PulesDbusClient::get_sources(&proxy, &conn)?;
+
+        Ok(Self {
+            conn,
+            sinks,
+            sources,
+        })
     }
 
     fn get_server_address() -> Result<String, Box<dyn std::error::Error>> {
@@ -45,29 +67,56 @@ impl PulesDbusClient {
         }
     }
 
-    pub fn get_sinks(&self) -> Result<Vec<ObjectPath>, Box<dyn std::error::Error>> {
-        // TODO Move to struct field
-        let core_proxy = Proxy::new(
-            &self.conn,
-            "org.PulseAudio1",
-            "/org/pulseaudio/server_lookup1",
-            "org.PulseAudio.ServerLookup1",
-        )?;
-        let sinks: Vec<ObjectPath> = core_proxy.get_property("Sinks")?;
+    // TODO Generics for devices
+    fn get_sinks(
+        proxy: &Proxy,
+        conn: &Connection,
+    ) -> Result<Vec<Sink<'a>>, Box<dyn std::error::Error>> {
+        let sink_paths: Vec<ObjectPath> = proxy.get_property("Sinks")?;
 
-        Ok(sinks)
+        let sink_vec: Vec<Sink> = Vec::new();
+        for sink_path in sink_paths {
+            let proxy = Proxy::new(
+                &conn,
+                "org.PulseAudio.Core1.Device",
+                &sink_path,
+                "org.PulseAudio.Core1.Device",
+            )?;
+
+            sink_vec.push(Sink {
+                path: sink_path,
+                name: proxy.get_property("Name")?,
+                volume: proxy.get_property("Volume")?,
+                mute: proxy.get_property("Mute")?,
+            })
+        }
+
+        Ok(sink_vec)
     }
 
-    pub fn get_sources(&self) -> Result<Vec<ObjectPath>, Box<dyn std::error::Error>> {
-        // TODO Move to struct field
-        let core_proxy = Proxy::new(
-            &self.conn,
-            "org.PulseAudio1",
-            "/org/pulseaudio/server_lookup1",
-            "org.PulseAudio.ServerLookup1",
-        )?;
-        let sinks: Vec<ObjectPath> = core_proxy.get_property("Sources")?;
+    fn get_sources(
+        proxy: &Proxy,
+        conn: &Connection,
+    ) -> Result<Vec<Source<'a>>, Box<dyn std::error::Error>> {
+        let sources_path: Vec<ObjectPath> = proxy.get_property("Sources")?;
 
-        Ok(sinks)
+        let source_vec: Vec<Source> = Vec::new();
+        for source_path in sources_path {
+            let proxy = Proxy::new(
+                &conn,
+                "org.PulseAudio.Core1.Device",
+                &source_path,
+                "org.PulseAudio.Core1.Device",
+            )?;
+
+            source_vec.push(Source {
+                path: source_path,
+                name: proxy.get_property("Name")?,
+                volume: proxy.get_property("Volume")?,
+                mute: proxy.get_property("Mute")?,
+            })
+        }
+
+        Ok(source_vec)
     }
 }
